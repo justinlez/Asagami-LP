00e789ca43f770a8d54536abef96f56328fc7ac0
Justin Lim <justin.lim.2024@computing.smu.edu.sg>
2026-05-28 18:24:22 +0900

    feat: created structure of project

---
+++ app/src/App.tsx
@@ -0,0 +1,13 @@
+import logo from '@/assets/Eletus Logo.jpg'
+function App() {
+  
+  return (
+    <div>
+      Home Page
+      <img src={logo} alt="" />
+    </div>
+    
+  )
+}
+
+export default App
