50ac645d561f8eff4b584dcd05574e1fa4fdb8c2
Justin Lim <justin.lim.2024@computing.smu.edu.sg>
2026-06-02 16:10:50 +0900

    chore: fixed routing

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,22 +1,16 @@
-import { Link, matchRoutes, Outlet, useLocation } from "react-router-dom";
+import { Navigate, Outlet, useLocation } from "react-router-dom";
 import { Navbar } from "./components/navbar";
 
 // import {Outlet} from "@tanstack/react-router"
 function App() {
   const url = useLocation();
-
+  if (url.pathname =='/'){
+    Navigate({to:'/home'})
+  }
   return (
     <div className="bg-background h-screen w-screen">
       <Navbar />
       <hr className="border-gray-300" />
-      {url.pathname == "/" && (
-        <Link
-          to="/home"
-          className="flex w-fit mx-auto items-center justify-center mt-[35vh]"
-        >
-          <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" type="button">Let's get started!</button>
-        </Link>
-      )}
       <Outlet />
     </div>
   );
