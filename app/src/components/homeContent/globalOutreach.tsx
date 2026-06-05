45dd2de1a7e3e6577cb6c354bfac7311dd8cda6e
Justin Lim <justin.lim.2024@computing.smu.edu.sg>
2026-06-05 18:04:16 +0900

    feat: global outreach section. added country and company logos. Refactored the file layout

---
+++ app/src/components/homeContent/globalOutreach.tsx
@@ -0,0 +1,75 @@
+import MapBackground from "@/assets/map.svg";
+import usa from "@/assets/countries/unitedstates.svg";
+import japan from "@/assets/countries/japan.svg";
+import philippines from "@/assets/countries/philippines.svg";
+import singapore from "@/assets/countries/singapore.svg";
+import taiwan from "@/assets/countries/taiwan.svg";
+import vietnam from "@/assets/countries/vietnam.svg";
+import daihatsu from "@/assets/companies/daihatsu.svg";
+import kyushuElect from "@/assets/companies/kyushuElect.svg";
+import nipponBank from "@/assets/companies/nipponBank.svg";
+import NTOU from "@/assets/companies/NTOU.svg";
+import { useState } from "react";
+
+export function GlobalOutreach() {
+  const [hoveredCountry, setHoveredCountry] = useState<string | null>(null);
+
+  const countries = [
+    { name: "USA", flag: usa },
+    { name: "Japan", flag: japan },
+    { name: "Philippines", flag: philippines },
+    { name: "Singapore", flag: singapore },
+    { name: "Taiwan", flag: taiwan },
+    { name: "Vietnam", flag: vietnam },
+  ];
+  const companies = [
+    { name: "Daihatsu", logo: daihatsu },
+    { name: "Kyushu Electric", logo: kyushuElect },
+    { name: "Nippon Bank", logo: nipponBank },
+    { name: "NTOU", logo: NTOU },
+  ];
+
+  return (
+    <div className="h-auto w-full bg-background">
+      <div className="text-gray-600 text-4xl 2xl:text-6xl font-bold text-center py-[3%]">
+        Our Global Outreach
+      </div>
+      {/* <img src={MapBackground} alt="" className="-"/> */}
+      <div
+        style={{ backgroundImage: `url(${MapBackground})` }}
+        className="flex flex-col items-center justify-center bg-cover bg-center bg-no-repeat h-[70vh] mb-[10vh]"
+      >
+        <div className="flex flex-row gap-[3%] justify-center w-3/4">
+          {countries.map((country) => (
+            <div className="" key={country.name}>
+              <img
+                src={country.flag}
+                alt={country.name}
+                className={`h-[20vh] block ${hoveredCountry === country.name ? "scale-120" : "scale-100"}`}
+                onMouseEnter={() => setHoveredCountry(country.name)}
+                onMouseLeave={() => setHoveredCountry(null)}
+              />
+              {hoveredCountry === country.name && (
+                <p className="text-center block text-xl font-notojp">
+                  {country.name}
+                </p>
+              )}
+            </div>
+          ))}
+        </div>
+        <div className="flex flex-row pt-[5vh] font-notojp text-md 2xl:text-xl">
+          Available in 14 languages!
+        </div>
+      </div>
+
+      <div className="flex flex-col items-center min-h-[30vh] gap-[2vh]">
+        <div className="flex flex-row 2xl:text-3xl font-bold font-space">We have worked with</div>
+        <div className="flex flex-row gap-[5%] justify-center">
+            {companies.map((company)=>(
+                <img src={company.logo} alt="" />
+            ))}
+        </div>
+      </div>
+    </div>
+  );
+}
---
+++ app/src/components/homeContent/homeMain.tsx
@@ -0,0 +1,110 @@
+import { Link } from "react-router-dom";
+import { CTAButton } from "@/components/ui/button";
+import { ArrowRightIcon, BookOpenCheck, Bot, ChartLine, Info, NotebookText } from "lucide-react";
+import homepg from '@/assets/homepage screen.svg'
+import { useHover } from "@/lib/utils";
+
+export function HomeMain(){
+    const { customRef, hovering } = useHover();
+    return(
+        <div className="flex flex-row w-[90vw] mx-auto gap-[1%]">
+        <div className="flex flex-col w-[40%]">
+          <div className="flex flex-col min-h-[90vh] justify-center">
+            <div className="text-4xl 2xl:text-7xl font-bold font-notojp">
+              Transform Knowledge Into Personalized Learning
+            </div>
+            <br />
+            <div className="text-base 2xl:text-2xl font-space pb-6">
+              Asagami converts notes, SOPs, videos, manuals, and teaching
+              materials into structured learning, adaptive quizzes, AI-supported
+              revision, and real-time learning insights.
+            </div>
+            <div>
+              <Link to="/features">
+                <CTAButton className="flex items-center justify-center gap-1 ps-6 text-base 2xl:text-2xl 2xl:py-[25px]">
+                  Discover Features <ArrowRightIcon className="inline" />
+                </CTAButton>
+              </Link>
+            </div>
+          </div>
+          <div className="mb-[15%]">
+            <div className="flex flex-row text-3xl 2xl:text-5xl font-bold font-notojp items-center gap-2 pb-10">
+              How Asagami AI Works
+              <Info className="inline text-gray-800" ref={customRef} />
+              <div className="flex items-center">
+                {hovering && (
+                  <div className="absolute bg-gray-300 z-40 font-normal font-sans rounded text-base p-3">
+                    Click on each feature to find out more
+                  </div>
+                )}
+              </div>
+            </div>
+            <div className="grid grid-cols-2 gap-x-[2%] gap-y-[6%]">
+              <div className="flex flex-col">
+                <Link to="/features" className='hover:shadow-xs rounded-lg hover:shadow-[0_0_20px_rgba(0,0,0,0.25)] p-[1%]'>
+                  <NotebookText
+                    className="text-leaf"
+                    size={45}
+                    strokeWidth={1.5}
+                  />
+                  <p className="font-semibold font-notojp text-xl 2xl:text-3xl py-[3%]">
+                    Intelligent Note Creation
+                  </p>
+                  <p className="text-base 2xl:text-lg">
+                    Customise clear and organized notes from scratch or from
+                    existing materials
+                  </p>
+                </Link>
+              </div>
+              <div className="flex flex-col">
+                <Link to="/features" className='hover:shadow-xs rounded-lg hover:shadow-[0_0_20px_rgba(0,0,0,0.25)] p-[1%]'>
+                  <BookOpenCheck
+                    className="text-leaf"
+                    size={45}
+                    strokeWidth={1.5}
+                  />
+                  <p className="font-semibold font-notojp text-xl 2xl:text-3xl py-[3%]">
+                    Personalized Quiz Design
+                  </p>
+                  <p className="text-base 2xl:text-lg">
+                    Automatically generate adaptive quizzes tailored to specific
+                    topics and learning goals.
+                  </p>
+                </Link>
+              </div>
+              <div className="flex flex-col">
+                <Link to="/features" className='hover:shadow-xs rounded-lg hover:shadow-[0_0_20px_rgba(0,0,0,0.25)] p-[1%]'>
+                  <ChartLine className="text-leaf" size={45} strokeWidth={1.5} />
+                  <p className="font-semibold font-notojp text-xl 2xl:text-3xl py-[3%]">
+                    Analytics
+                  </p>
+                  <p className="text-base 2xl:text-lg">
+                    Track learner progress, weak points, engagement, and
+                    performance in real time.
+                  </p>
+                </Link>
+              </div>
+              <div className="flex flex-col">
+                <Link to="/features" className='hover:shadow-xs rounded-lg hover:shadow-[0_0_20px_rgba(0,0,0,0.25)] p-[1%]' >
+                  <Bot className="text-leaf" size={45} strokeWidth={1.5} />
+                  <p className="font-semibold font-notojp text-xl 2xl:text-3xl py-[3%]">
+                    Chatbot
+                  </p>
+                  <p className="text-base 2xl:text-lg">
+                    Provide instant chatbot guidance and clarification whenever
+                    learners need help.
+                  </p>
+                </Link>
+              </div>
+            </div>
+          </div>
+        </div>
+        {/* RIGHT COLUMN — sticky image */}
+        <div className="flex flex-col w-auto mx-auto pb-[4%]">
+          <div className="sticky top-[20%] flex justify-center">
+            <img src={homepg} alt="" className="h-[70vh] w-auto" />
+          </div>
+        </div>
+      </div>
+    )
+}
