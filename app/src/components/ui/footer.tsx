4722f530c30f5d96caaf9440370a66190cb34233
Justin Lim <justin.lim.2024@computing.smu.edu.sg>
2026-06-08 15:30:41 +0900

    chore: fixed footer

--- app/src/components/ui/footer.tsx
+++ app/src/components/ui/footer.tsx
@@ -1,14 +1,16 @@
 import { Copyright } from "lucide-react";
+import { Link } from "react-router-dom";
 
 export function Footer(){
+
     return (
         <div>
             <hr className="border-gray-300" />
-            <div className="flex items-center h-[3vh] py-[3%]  w-[90vw] mx-auto justify-between text-gray-600 text-sm 2xl:text-base">
+            <div className="flex items-center h-[3vh] py-[3%]  w-[90vw] mx-auto justify-between text-gray-500 text-sm 2xl:text-base">
                 <div className="flex items-center gap-1"><Copyright className="flex-inline items-center" size={20}/> Eletus Inc.</div>
                 <div className="flex gap-12 w-auto">
-                    <p className="flex">Privacy Policy</p>
-                    <p className="flex w-auto">Disclosure based on the Specified Commercial Transactions Act</p>
+                    <Link to='https://www.eletus.co.jp/privacy-policy-2/' target="_blank" className='hover:text-leaf'>Privacy Policy</Link>
+                    <Link to='https://www.eletus.co.jp/act-on-specified-commercial-transactions/' target="_blank" className='hover:text-leaf'>Disclosure based on the Specified Commercial Transactions Act</Link>
                 </div>
             </div>
         </div>
